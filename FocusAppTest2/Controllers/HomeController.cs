ab5b27551c820a6db6d5f08a4fd661a505e7261a
arash87 <arash_farmani@msn.com>
2015-04-25 10:54:40 +0200

    dd

--- FocusAppTest2/Controllers/HomeController.cs
+++ FocusAppTest2/Controllers/HomeController.cs
@@ -13,8 +13,8 @@ namespace FocusAppTest2.Controllers
     {
         //
         // GET: /Home/
-        ServiceReference1.Service1Client obj = new ServiceReference1.Service1Client();      //published
-        //ServiceReference2.Service1Client obj = new ServiceReference2.Service1Client();    //localhost
+        //ServiceReference1.Service1Client obj = new ServiceReference1.Service1Client();      //published
+        ServiceReference2.Service1Client obj = new ServiceReference2.Service1Client();    //localhost
 
         public ActionResult Index()
         {
